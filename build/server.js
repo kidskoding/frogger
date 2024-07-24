c4b4464084aac345c148fe2fb3f56d9e1eece2eb
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-07-24 15:08:51 -0500

    finalized build of game

--- build/server.js
+++ build/server.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', async (req, res) => {
     try {
